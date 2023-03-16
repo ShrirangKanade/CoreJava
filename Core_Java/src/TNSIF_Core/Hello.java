b591cb39d5b2a7bd762a927c1f9ba652f049d056
Admin <Admin@192.168.1.9>
2023-03-16 20:31:31 +0530

    2nd commit

--- Core_Java/src/TNSIF_Core/Hello.java
+++ Core_Java/src/TNSIF_Core/Hello.java
@@ -1,10 +1,23 @@
 package TNSIF_Core;
-
+import java.util.*;
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int sum=0,num,rem;
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		while(true)
+		{ 
+			rem = num%10;
+			if (rem==0)
+				break;
+			sum=sum*10+rem;
+			num =num/10;
+		}
+		System.out.println(sum);
 
 	}
-
+	
 }
